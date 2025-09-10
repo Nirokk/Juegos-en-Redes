f53061701934146cc590068dfc6e872b8c119ebc
Ifritdo <JulianFirpo316@hotmail.com>
2025-09-09 23:25:15 -0300

    Commit ScriptableObjects
    Se creo Scriptable Objects para Balas y Armas.
    Actualmente, el jugador se le creo tambien un script WeaponHandler para que este pueda disparar una pistola
    Faltaria agregar los prefabs de las Balas y armas (Esto deseo discutir si vale la pena por el trafico de red)

---
+++ Juegos en Red/Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class Bullet : MonoBehaviourPun
+{
+    private float speed;
+    private float damage;
+    private float lifetime;
+
+    public void Initialize(BulletData data)
+    {
+        speed = data.speed;
+        damage = data.damage;
+        lifetime = data.lifetime;
+
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!photonView.IsMine) return; // Solo el dueño procesa el impacto
+
+        // Ejemplo: si choca contra un jugador
+        var target = collision.GetComponent<Player_Model>();
+        if (target != null)
+        {
+            target._currentLife -= (int)damage;
+        }
+
+        PhotonNetwork.Destroy(gameObject);
+    }
+}
