0a9ad62035caaeb861278c23d0a77e1ffef610c5
Ifritdo <JulianFirpo316@hotmail.com>
2025-11-18 20:48:02 -0300

    commit Hud granada

---
+++ Juegos en Red/Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerHUD : MonoBehaviour
+{
+    public Image grenadeIcon;
+    private Player_Inventory inv;
+    private PhotonView pv;
+
+    void Start()
+    {
+        pv = GetComponentInParent<PhotonView>();
+
+        // Si NO es mi jugador = oculto el HUD
+        if (!pv.IsMine)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        // Buscar inventario DENTRO de mi player
+        inv = GetComponentInParent<Player_Inventory>();
+    }
+
+    void Update()
+    {
+        if (inv != null)
+            grenadeIcon.enabled = inv.hasGrenade;
+    }
+}
