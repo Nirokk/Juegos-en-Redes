7f0418c34542990fa0770d764f806dc70c103c9b
Nirokk <funaronicolas@gmail.com>
2025-11-19 22:56:05 -0300

    Players can leave the room

--- Juegos en Red/Assets/Scripts/TeamSelection/NoTeamPanel.cs
+++ Juegos en Red/Assets/Scripts/TeamSelection/NoTeamPanel.cs
@@ -1,55 +1,65 @@
 using Photon.Realtime;
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoTeamPanel : MonoBehaviour
+public class NoTeamPanel : MonoBehaviourPunCallbacks
 {
     [SerializeField] private PlayerIcon playerPrefab;
     [SerializeField] private Transform noTeamPanel;
     [SerializeField] private Transform aTeamPanel;
     [SerializeField] private Transform bTeamPanel;
-    
 
     private List<PlayerIcon> playersUI = new List<PlayerIcon>();
     private const string TEAM_KEY = "team";
 
     void Start()
     {
-      
-        NetworkManager.Instance.OnJoinedRoom += UpdatePlayers;
-        NetworkManager.Instance.OnPlayerLeftRoom += UpdatePlayers;
-        NetworkManager.Instance.OnPlayerEnteredRoom += UpdatePlayers;
-
         UpdatePlayers();
     }
 
-    private void Update()
+    // Cuando un jugador cambia sus custom properties (como team)
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey(TEAM_KEY))
+        {
+            UpdatePlayers();
+        }
+    }
+
+    // Cuando un jugador entra a la room
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         UpdatePlayers();
     }
+
+    // Cuando un jugador sale de la room
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayers();
+    }
+
+
     private void UpdatePlayers()
     {
-
         ClearPlayers();
 
-        Dictionary<int, Player> players = NetworkManager.Instance.GetPlayersInRoom();
-        Debug.Log("Players count: " + players.Count);
+        Player[] players = PhotonNetwork.PlayerList;
+        Debug.Log("PhotonNetwork.PlayerList count = " + players.Length);
 
-        foreach (KeyValuePair<int, Player> kv in players)
+        foreach (Player p in players)
         {
-            Player p = kv.Value;
+            if (p == null) continue;
+            if (string.IsNullOrEmpty(p.NickName)) continue;   // evita íconos vacíos
+
             string team = null;
 
             if (p.CustomProperties.TryGetValue(TEAM_KEY, out object teamObj))
-            {
                 team = teamObj?.ToString();
-                Debug.Log($"Player {p.NickName} is in team {team}");
-
-            }
-
 
             PlayerIcon icon;
+
             if (team == "A")
             {
                 icon = Instantiate(playerPrefab, aTeamPanel);
@@ -68,23 +78,16 @@ public class NoTeamPanel : MonoBehaviour
             icon.SetUp(p);
             playersUI.Add(icon);
         }
-
-
     }
 
     private void ClearPlayers()
     {
-        //foreach (PlayerIcon playerItemUI in playersUI)
-        //{
-        //    Destroy(playerItemUI.gameObject);
-        //}
-        //playersUI.Clear();
-
         foreach (PlayerIcon playerItemUI in playersUI)
         {
             if (playerItemUI != null)
                 Destroy(playerItemUI.gameObject);
         }
+
         playersUI.Clear();
     }
 }
---
+++ Juegos en Red/Assets/Scripts/TeamSelection/TeamSelectionManager.cs
@@ -0,0 +1,105 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+
+public class TeamSelectionManager : MonoBehaviourPunCallbacks
+{
+    public Button startMatch;
+    public Button joinTeamA;
+    public Button joinTeamB;
+    public Button returnButton;
+    
+    private ExitGames.Client.Photon.Hashtable _myTeamIs = new ExitGames.Client.Photon.Hashtable();
+
+
+
+
+    void Start()
+    {
+        startMatch.onClick.AddListener(StartGame);
+        joinTeamA.onClick.AddListener(AddToTeamA);
+        joinTeamB.onClick.AddListener(AddToTeamB);
+        returnButton.onClick.AddListener(LeaveRoom);
+        
+
+        startMatch.interactable = PhotonNetwork.IsMasterClient;
+
+        CheckTeamsAndUpdateStartButton();
+    }
+
+
+    private void StartGame()
+    {
+        NetworkManager.Instance.LoadSceneForEveryone("GameScene");
+        
+    }
+
+
+    public void AddToTeamA()
+    {
+        _myTeamIs["team"] = "A";
+        PhotonNetwork.LocalPlayer.SetCustomProperties(_myTeamIs);
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " moved to Team A");
+        CheckTeamsAndUpdateStartButton();
+    }
+
+    
+    public void AddToTeamB()
+    {
+        _myTeamIs["team"] = "B";
+        PhotonNetwork.LocalPlayer.SetCustomProperties(_myTeamIs);
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " moved to Team B");
+        CheckTeamsAndUpdateStartButton();
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("LobbyScene");
+    }
+    // Se llama cuando cambian propiedades (como team)
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        CheckTeamsAndUpdateStartButton();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        otherPlayer.CustomProperties["team"] = null;
+        CheckTeamsAndUpdateStartButton();
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startMatch.interactable = PhotonNetwork.IsMasterClient;
+        CheckTeamsAndUpdateStartButton();
+    }
+
+    private void CheckTeamsAndUpdateStartButton()
+    {
+        int teamAcount = 0;
+        int teamBcount = 0;
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            if (p.CustomProperties.TryGetValue("team", out object teamObj))
+            {
+                string t = (string)teamObj;
+                if (t == "A") teamAcount++;
+                if (t == "B") teamBcount++;
+            }
+        }
+
+        bool bothTeamsHavePlayers = teamAcount > 0 && teamBcount > 0;
+
+        
+        startMatch.interactable = PhotonNetwork.IsMasterClient && bothTeamsHavePlayers;
+    }
+}
+
